c66b07e8db67836812fae390c7250efe59459e28
Ropestan <roope.tukiainen@gmail.com>
2024-02-26 15:25:30 +0200

    mycrypt.py, encode doesn't check invalid characters issue fixed

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -3,21 +3,32 @@ import codecs
 def encode(s):
     if not isinstance(s,str):
         raise TypeError
+    valid_chars = set
+    (
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", 
+        "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7",
+        "8", "9", "!", '"', "#", "€", "%", "&", "/", "(", ")", "=", "'"
+    )
     origlen = len(s)
     crypted = ""
-    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    non_letter_mapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
     for c in s:
         if c.isalpha():
+            if not c.lower() in valid_chars:
+                raise ValueError
             if c.islower():
                 c=c.upper()
             else:
                 c=c.lower()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
-        elif c in digitmapping:
-          crypted+=digitmapping[c]
+        else:
+            if not c in valid_chars:
+                raise ValueError
+            crypted+=non_letter_mapping[c]
 
     return crypted
 
